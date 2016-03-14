2f607b85dff2655d814a6fc9d2f021da008eedd1
Jim Baker <jamesmbaker@gmail.com>
2016-03-13 22:06:19 -0400

    Initial commit of watchface.

---
+++ src/simple_analog.c
@@ -0,0 +1,177 @@
+#include "simple_analog.h"
+
+#include "pebble.h"
+
+static Window *s_window;
+static Layer *s_simple_bg_layer, *s_date_layer, *s_hands_layer;
+static TextLayer *s_day_label, *s_num_label;
+static BitmapLayer *background_image_layer;
+static GBitmap *background_image;
+
+static GPath *s_tick_paths[NUM_CLOCK_TICKS];
+static GPath *s_minute_arrow, *s_hour_arrow, *s_second_arrow;
+static char s_num_buffer[4], s_day_buffer[6];
+
+static void bg_update_proc(Layer *layer, GContext *ctx) {
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
+    const int x_offset = PBL_IF_ROUND_ELSE(18, 0);
+    const int y_offset = PBL_IF_ROUND_ELSE(6, 0);
+    gpath_move_to(s_tick_paths[i], GPoint(x_offset, y_offset));
+    gpath_draw_filled(ctx, s_tick_paths[i]);
+  }
+}
+
+static void hands_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GPoint center = grect_center_point(&bounds);
+    
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+
+  // minute/hour hand
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+
+  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
+  gpath_draw_filled(ctx, s_minute_arrow);
+  //gpath_draw_outline(ctx, s_minute_arrow);
+
+  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
+  gpath_draw_filled(ctx, s_hour_arrow);
+  //gpath_draw_outline(ctx, s_hour_arrow);
+    
+  // second hand
+  graphics_context_set_fill_color(ctx, GColorDarkCandyAppleRed);
+  graphics_context_set_stroke_color(ctx, GColorDarkCandyAppleRed);
+    
+  gpath_rotate_to(s_second_arrow, TRIG_MAX_ANGLE * t->tm_sec / 60);
+  gpath_draw_filled(ctx, s_second_arrow);
+  //gpath_draw_outline(ctx, s_second_arrow);
+    
+  // dots in the middle
+  graphics_context_set_fill_color(ctx, GColorDarkCandyAppleRed);
+  graphics_fill_circle(ctx, center, 6);
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_circle(ctx, center, 2);
+}
+
+static void date_update_proc(Layer *layer, GContext *ctx) {
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+
+  strftime(s_day_buffer, sizeof(s_day_buffer), "%a", t);
+  text_layer_set_text(s_day_label, s_day_buffer);
+
+  strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
+  text_layer_set_text(s_num_label, s_num_buffer);
+}
+
+static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
+  layer_mark_dirty(window_get_root_layer(s_window));
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_simple_bg_layer = layer_create(bounds);
+  layer_set_update_proc(s_simple_bg_layer, bg_update_proc);
+  layer_add_child(window_layer, s_simple_bg_layer);
+
+  // Background image
+  background_image_layer = bitmap_layer_create(bounds);
+  background_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  bitmap_layer_set_compositing_mode(background_image_layer, GCompOpAssign);
+  bitmap_layer_set_bitmap(background_image_layer, background_image);
+  bitmap_layer_set_alignment(background_image_layer, GAlignCenter);
+  layer_add_child(s_simple_bg_layer, bitmap_layer_get_layer(background_image_layer));
+    
+  s_date_layer = layer_create(bounds);
+  layer_set_update_proc(s_date_layer, date_update_proc);
+  layer_add_child(window_layer, s_date_layer);
+
+  s_day_label = text_layer_create(GRect(5, 77, 27, 20));
+  text_layer_set_text(s_day_label, s_day_buffer);
+  text_layer_set_background_color(s_day_label, GColorWhite);
+  text_layer_set_text_color(s_day_label, GColorBlack);
+  text_layer_set_font(s_day_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+
+  layer_add_child(s_date_layer, text_layer_get_layer(s_day_label));
+
+  s_num_label = text_layer_create(GRect(160, 77, 18, 20));
+  text_layer_set_text(s_num_label, s_num_buffer);
+  text_layer_set_background_color(s_num_label, GColorWhite);
+  text_layer_set_text_color(s_num_label, GColorBlack);
+  text_layer_set_font(s_num_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+
+  layer_add_child(s_date_layer, text_layer_get_layer(s_num_label));
+
+  s_hands_layer = layer_create(bounds);
+  layer_set_update_proc(s_hands_layer, hands_update_proc);
+  layer_add_child(window_layer, s_hands_layer);
+}
+
+static void window_unload(Window *window) {
+  gbitmap_destroy(background_image);
+  bitmap_layer_destroy(background_image_layer);
+    
+  layer_destroy(s_simple_bg_layer);
+  layer_destroy(s_date_layer);
+
+  text_layer_destroy(s_day_label);
+  text_layer_destroy(s_num_label);
+
+  layer_destroy(s_hands_layer);
+}
+
+static void init() {
+  s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(s_window, true);
+
+  s_day_buffer[0] = '\0';
+  s_num_buffer[0] = '\0';
+
+  // init hand paths
+  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
+  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
+  s_second_arrow = gpath_create(&SECOND_HAND_POINTS);
+
+  Layer *window_layer = window_get_root_layer(s_window);
+  GRect bounds = layer_get_bounds(window_layer);
+  GPoint center = grect_center_point(&bounds);
+  gpath_move_to(s_minute_arrow, center);
+  gpath_move_to(s_hour_arrow, center);
+  gpath_move_to(s_second_arrow, center);
+    
+  //for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
+  //  s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]);
+  //}
+
+  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+}
+
+static void deinit() {
+  gpath_destroy(s_minute_arrow);
+  gpath_destroy(s_hour_arrow);
+  gpath_destroy(s_second_arrow);
+
+  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
+    gpath_destroy(s_tick_paths[i]);
+  }
+
+  tick_timer_service_unsubscribe();
+  window_destroy(s_window);
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
